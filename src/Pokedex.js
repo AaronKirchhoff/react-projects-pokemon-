4771cb329fd045fcd440bb385311d3ae907cf4ee
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-07-05 14:34:49 -0700

    Monday-July-5-commit

---
+++ src/Pokedex.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+// interesting, after import you call this import any name, but it MUST be capitolized and used as uppercase thorughout this component. notice on line 27 I called it Pokecard
+import Pokecard from './pokecard';
+import './Pokedex.css';
+
+
+class Pokedex extends Component{
+    // static deafault props is like setting variable names for your props so you dont have to write the whole thing out everytime you want to call a prop. its helpful.
+    // static defaultProps = { ... i mived the array of all the pokemon instances to pokegame for some reason.
+    
+    render(){
+        let title;
+        if(this.props.isWinner){
+            title = <h1 className="Pokedex-winner">Winning Hand!</h1>
+        } else {
+            title = <h1 className="Pokedex-loser">Losing Hand!</h1>
+        }
+
+        return (
+            <div className= "Pokedex">
+                {title}
+                <h4>Total Expereince: {this.props.exp}</h4>
+                {/* notice the below class name, its conventional to give a prefix of the parent div before naming it */}
+                <div className="Pokedex-cards">
+                    {this.props.pokemon.map((p) => (
+                        <Pokecard id={p.id} name={p.name} type={p.type} exp= {p.base_experience} />
+                    ))}
+
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default Pokedex;
---
+++ src/pokecard.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './pokecard.css';
+// const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+// this line of code changes the id number given to a three digit number. we do this because of the img source http requirments. can always get images from somewhere else though.
+let padToThree = (number) => (number <= 999 ? `00${number}` .slice(-3): number);
+
+
+// so here is our pokecar component. it needs to accept props anytime we want to call the component right? like it has parameters to function. anytime we call this component in our App.js we need to pass arguments, or props. so in App.js we give props, here in the component pokecard, we receive props.
+class Pokecard extends Component {
+    render() {
+        // back ticks `` means interpolate ??
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+        return (
+            <div className="Pokecard">
+                <h1 className="Pokecard-title">{this.props.name}</h1>
+                <div className="Pokecard-image">
+                    <img src= {imgSrc} alt={this.props.name}/>
+                </div>
+                <div className="Pokecard-data"> Type: {this.props.type} </div>
+                <div className="Pokecard-data"> Experience: {this.props.exp} </div>
+                <div className="Pokecard-data"> Creature ID: {this.props.id} </div>
+
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
